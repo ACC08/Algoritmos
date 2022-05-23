e4b4bed9d96ba7fe7042bcddee0cc363979c160a
ACC08 <89225039+ACC08@users.noreply.github.com>
2022-05-23 13:11:47 -0300

    Add files via upload

---
+++ Cuaderno/Blanco 1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ahcde
+ * Date: 12/4/2022
+ * Time: 16:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Blanco_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Introduzca una serie de numeros");
+			string numeros = Console.ReadLine();
+			foreach (char numero in numeros){
+				Console.WriteLine(numero);
+				
+			}
+			Console.ReadKey();
+		}
+	}
+}
